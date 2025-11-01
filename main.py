8a62d0b0ffbce3f0dcbfaf5f7e5cd81a14e83d4b
GrantC <grantcaile0@gmail.com>
2025-11-01 11:36:19 -0400

    Added all table info to pandas dataframe

--- main.py
+++ main.py
@@ -10,7 +10,21 @@ soap = BeautifulSoup(page.text, "html.parser")
 table = soap.find("table", id="main-table")
 t_titles = soap.find_all("th")
 table_titles = [titles.get_text(strip=True) for titles in t_titles]
-data_frame = pd.DataFrame(columns = table_titles)
+table_titles.pop(0)
 info_data = soap.find_all("td")
 info_txt = [info.get_text(strip=True) for info in info_data]
-print(info_txt)
+data = []
+start = 0
+end = 7
+while end != 3507:
+    data.append(info_txt[start:end])
+    start = end
+    end+=7
+row_data = []
+for d in data:
+   d.pop(0)
+df = pd.DataFrame(data, columns = table_titles)
+
+
+    
+print(df)
