6033c75f31952753d67c556f9572ec48bc0fe348
GrantC <grantcaile0@gmail.com>
2025-11-01 16:49:46 -0400

    Abstracted and made it so changing table length doesnt mess up whole thing

--- main.py
+++ main.py
@@ -2,29 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+def webscraper(site):
+    url = site                                    
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    return soup
 
-url =  "https://stockanalysis.com/list/highest-revenue/"                                        #"https://sandbox.oxylabs.io/products" for dynamic practice 
-page = requests.get(url)
-soap = BeautifulSoup(page.text, "html.parser")
-#print(soap.prettify())
-table = soap.find("table", id="main-table")
-t_titles = soap.find_all("th")
-table_titles = [titles.get_text(strip=True) for titles in t_titles]
-table_titles.pop(0)
-info_data = soap.find_all("td")
-info_txt = [info.get_text(strip=True) for info in info_data]
-data = []
-start = 0
-end = 7
-while end != 3507:
-    data.append(info_txt[start:end])
-    start = end
-    end+=7
-row_data = []
-for d in data:
-   d.pop(0)
-df = pd.DataFrame(data, columns = table_titles)
-
-
+def clean_data(soup):
+    t_titles = soup.find_all("th")
+    table_titles = [titles.get_text(strip=True) for titles in t_titles]
+    table_titles.pop(0)
+    info_data = soup.find_all("td")
+    info_txt = [info.get_text(strip=True) for info in info_data]
+    return info_txt, table_titles
     
-print(df)
+def get_rows(info_txt,width):
+    data = []
+    start = 0
+    end = len(info_txt) + 1
+    while start + width < end:
+        data.append(info_txt[start:start + width])
+        start+=width
+    return data 
+
+def rid_of_numbers(data):
+    for d in data:
+        d.pop(0)
+    return data 
+
+def create_df(data, table_titles):
+    df = pd.DataFrame(data, columns = table_titles)
+    return df 
+
+def main():
+    raw_data = webscraper("https://stockanalysis.com/list/highest-revenue/")
+    cleaned_data, titles = clean_data(raw_data)
+    width = len(titles) + 1
+    rows = get_rows(cleaned_data,width)
+    data = rid_of_numbers(rows)
+    df = create_df(data, titles)
+    print(df)
+
+main()
