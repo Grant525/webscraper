a85964d2de6e0bf08c42d2f3a54d914262b8a38b
GrantC <grantcaile0@gmail.com>
2025-10-31 21:49:49 -0400

    Changed too harder site and grabed the information of a table

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import requests 
 from bs4 import BeautifulSoup
+import pandas as pd
 
-url =  "https://www.scrapethissite.com/pages/"                                        #"https://sandbox.oxylabs.io/products" for dynamic practice 
+
+url =  "https://stockanalysis.com/list/highest-revenue/"                                        #"https://sandbox.oxylabs.io/products" for dynamic practice 
 page = requests.get(url)
 soap = BeautifulSoup(page.text, "html.parser")
 #print(soap.prettify())
-soap.find("div")
+table = soap.find("table", id="main-table")
+t_titles = soap.find_all("th")
+table_titles = [titles.get_text(strip=True) for titles in t_titles]
+data_frame = pd.DataFrame(columns = table_titles)
+info_data = soap.find_all("td")
+info_txt = [info.get_text(strip=True) for info in info_data]
+print(info_txt)
